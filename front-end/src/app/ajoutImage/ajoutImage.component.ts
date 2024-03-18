e5d1f1ca59c94b112233f2d55e398c3ad161acc5
CamilleAntonios <camille.antonios@gmail.com>
2024-03-18 11:44:09 +0100

    #5 feat: 1ere transformation de la maquette matrio en angular

---
+++ front-end/src/app/ajoutImage/ajoutImage.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-ajoutImage',
+    templateUrl: './ajoutImage.component.html',
+    styleUrls: ['./ajoutImage.component.scss']
+})
+
+export class AjoutImage implements OnInit {
+    constructor() {}
+    ngOnInit(): void {}
+} 
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyHeader } from './header/header.component';
+import { AjoutImage } from './ajoutImage/ajoutImage.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    MyHeader, 
+    AjoutImage
   ],
   imports: [
     BrowserModule,
