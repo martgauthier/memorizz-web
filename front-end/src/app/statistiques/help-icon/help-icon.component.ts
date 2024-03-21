c1abc6b16fe09692b8ba9c2c4f88129734b78601
Gauthier MARTIN <45541503+martgauthier@users.noreply.github.com>
2024-03-21 13:30:32 +0100

    feat: added help-icon and monitoring-icon components. References #6

---
+++ front-end/src/app/statistiques/help-icon/help-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-icon',
+  templateUrl: './help-icon.component.html',
+  styleUrl: './help-icon.component.scss'
+})
+export class HelpIconComponent {
+
+}
