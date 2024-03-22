849180067d64e8266b2a163fd9210f19d8fcebbd
Gwendolyne Bouchard <gwendolyne.bouchard@etu.univ-cotedazur.fr>
2024-03-22 11:24:09 +0100

    #16 changement diff avec nb cartes

--- front-end/src/app/config/template.component.ts
+++ front-end/src/app/config/template.component.ts
@@ -17,29 +17,16 @@ export class Template implements OnInit {
 
     }
     public onclick_difficulties(id : string){
-        if(this.beginning){
-            (document.querySelector(".template") as HTMLDivElement)!.style.animationPlayState = "running";
-            this.beginning = false;
-            (document.querySelector("#jouer") as HTMLDivElement)!.style.display ="block";
-            document.querySelector("#jouer div")!.classList.add("jouer");
-            document.querySelector("#jouer div h3")!.classList.add("jouer_txt");
-            document.querySelector("#jouer div h3")!.classList.add("cocher");
-            document.querySelector("#jouer div h3")!.classList.remove("pas_cocher");
-        }
-        let difficulties: string[] = ['facile', 'moyen', 'difficile'];
-        let difficulty : string;
-        for(difficulty in difficulties){
-          if(difficulties[difficulty] == id ){
-            (document.querySelector("#"+id+" div") as HTMLDivElement)!.style.backgroundColor = "#01274a" ;
-            document.querySelector("#"+id+">div>h3")!.classList.add("cocher");
-            document.querySelector("#"+id+">div>h3")!.classList.remove("pas_cocher");
-            new Niveau(id,this.configService);
-          }
-          else{
-            (document.querySelector("#"+difficulties[difficulty]+" div") as HTMLDivElement)!.style.backgroundColor = "#209188";
-            document.querySelector("#"+difficulties[difficulty]+">div>h3")!.classList.add("pas_cocher");
-            document.querySelector("#"+difficulties[difficulty]+">div>h3")!.classList.remove("cocher");
-            }
-        }
+      if(this.beginning){
+        (document.querySelector(".template") as HTMLDivElement)!.style.animationPlayState = "running";
+        this.beginning = false;
+        (document.querySelector("#jouer") as HTMLDivElement)!.style.display ="block";
+        document.querySelector("#jouer div")!.classList.add("jouer");
+        document.querySelector("#jouer div h3")!.classList.add("jouer_txt");
+        document.querySelector("#jouer div h3")!.classList.add("cocher");
+        document.querySelector("#jouer div h3")!.classList.remove("pas_cocher");
+      }
+      new Niveau(id,this.configService);
+      this.configService.setFrontDifficulties(id);
       }
 } 
--- front-end/src/services/config-service.service.ts
+++ front-end/src/services/config-service.service.ts
@@ -12,10 +12,18 @@ export class ConfigService {
     onstructor() {}
 
     public setNbCard(value : number){
-        this.nbCard = value;
-        (document.querySelector(".input-number") as HTMLInputElement).value = value +"";
-        if(value <= 4 && this.niveau != "facile"){
-            new Niveau("facile",this);
+        if(value >= 1 && value <= 8){
+            this.nbCard = value;
+            (document.querySelector(".input-number") as HTMLInputElement).value = value +"";
+            if(value <= 3 && this.niveau != "facile"){
+                this.setFrontDifficulties("facile");
+            }
+            if(value >= 4 && value <= 6 && this.niveau != "moyen"){
+                this.setFrontDifficulties("moyen");
+            }
+            if(value >= 7 && this.niveau != "difficile"){
+                this.setFrontDifficulties("difficile");
+            }
         }
     }
     public setPosition(cachees : boolean){
@@ -62,5 +70,22 @@ export class ConfigService {
     }
     public setNiveau(niveau : string){
         this.niveau = niveau;
+    }
+    public setFrontDifficulties(id : string){
+        this.niveau = id;
+        let difficulties: string[] = ['facile', 'moyen', 'difficile'];
+        let difficulty : string;
+        for(difficulty in difficulties){
+          if(difficulties[difficulty] == id ){
+            (document.querySelector("#"+id+" div") as HTMLDivElement)!.style.backgroundColor = "#01274a" ;
+            document.querySelector("#"+id+">div>h3")!.classList.add("cocher");
+            document.querySelector("#"+id+">div>h3")!.classList.remove("pas_cocher");
+          }
+          else{
+            (document.querySelector("#"+difficulties[difficulty]+" div") as HTMLDivElement)!.style.backgroundColor = "#209188";
+            document.querySelector("#"+difficulties[difficulty]+">div>h3")!.classList.add("pas_cocher");
+            document.querySelector("#"+difficulties[difficulty]+">div>h3")!.classList.remove("cocher");
+            }
+        }
     }
 }
