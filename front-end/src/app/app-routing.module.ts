837f7d34016861a7f500be0e954a20fcf1370e7d
Gauthier MARTIN <45541503+martgauthier@users.noreply.github.com>
2024-03-25 21:28:03 +0100

    feature: added CardsOrGameSelector, and a routing system in statistiques. Added necessary components. Closes #44

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -4,10 +4,23 @@ import { Template } from './config/template.component';
 import { StatistiquesDisplayerComponent } from './statistiques/statistiques-displayer/statistiques-displayer.component';
 import { PageNavComponent } from './nav/pageNav.component';
 import { AjoutImage } from './ajoutImage/ajoutImage.component';
+import {
+  CardsDataContainerComponent
+} from "./statistiques/cards-data-container/cards-data-container/cards-data-container.component";
+import {GamesDataContainerComponent} from "./statistiques/games-data-container/games-data-container/games-data-container.component";
 
 const routes: Routes = [
   {path : "play", component: Template},
-  {path : "stats", component: StatistiquesDisplayerComponent},
+  {path : "stats", component: StatistiquesDisplayerComponent, children: [
+      {
+        path: "cardsStats",
+        component: CardsDataContainerComponent
+      },
+      {
+        path: "gamesStats",
+        component: GamesDataContainerComponent
+      }
+    ]},
   {path : "nav", component : PageNavComponent},
   {path : "ajoutImage", component : AjoutImage},
   {path: '', redirectTo : '/nav', pathMatch : "full"}
---
+++ front-end/src/app/statistiques/cards-data-container/cards-data-container/cards-data-container.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-data-container',
+  templateUrl: './cards-data-container.component.html',
+  styleUrl: './cards-data-container.component.scss'
+})
+export class CardsDataContainerComponent {
+}
