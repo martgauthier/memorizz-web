a2adf3a211ce6ab78aa212fa2831506c44cfd046
Gauthier MARTIN <45541503+martgauthier@users.noreply.github.com>
2024-04-13 22:23:31 +0200

    feat: disabled ability to open statistiques without a selected user.

--- front-end/src/app/statistiques/statistiques-displayer/statistiques-displayer.component.ts
+++ front-end/src/app/statistiques/statistiques-displayer/statistiques-displayer.component.ts
@@ -1,23 +1,33 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {Identification} from "../../../models/user.model";
 import {UserService} from "../../../services/user/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-statistiques-displayer',
   templateUrl: './statistiques-displayer.component.html',
   styleUrl: './statistiques-displayer.component.scss'
 })
-export class StatistiquesDisplayerComponent {
+export class StatistiquesDisplayerComponent implements AfterViewInit {
   public user: Identification = {
-    id: 0,
+    id: -1,
     prenom: "",
     nom: "",
     src : ""
   };
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
     userService.identification$.subscribe((identification) => {
       this.user=identification;
+      if(identification.id===-1) {
+        router.navigate(["/nav"]);
+      }
     });
+  }
+
+  ngAfterViewInit() {
+    if(this.user.id===-1) {
+      this.router.navigate(["/nav"]);
+    }
   }
 }
