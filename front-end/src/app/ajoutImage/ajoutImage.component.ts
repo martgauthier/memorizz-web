daf75384ba425530d90e6e09631c286ab2dd0572
Gauthier MARTIN <45541503+martgauthier@users.noreply.github.com>
2024-04-14 17:04:01 +0200

    feat: added ability to remove picture

--- front-end/src/app/ajoutImage/ajoutImage.component.ts
+++ front-end/src/app/ajoutImage/ajoutImage.component.ts
@@ -35,15 +35,21 @@ export class AjoutImage implements OnInit {
         if(event.target != null){
             if (event.target.files && event.target.files[0]) {
                 const file = event.target.files[0];
-        
+
                 const reader = new FileReader();
                 reader.onload = e => this.imageSrc = reader.result;
-        
+
                 reader.readAsDataURL(file);
 
                 const myPreview = document.getElementById("preview");
                 myPreview?.classList.add("loaded");
             }
         }
-    }    
-} 
+    }
+
+    onItemClick(card: Card) {
+      let currentCards=this.availableCards;
+      currentCards.splice(this.availableCards.indexOf(card), 1);
+      this.userService.availableCards$.next(currentCards);
+    }
+}
