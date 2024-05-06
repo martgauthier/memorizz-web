f87d67415439c9cd6cc03482f27500081488a9d1
AmelieMuller <amelie17.muller@gmail.com>
2024-05-06 11:59:17 +0200

    #141 fix : petits rÃegales

--- backend/app/models/user/card.model.js
+++ backend/app/models/user/card.model.js
@@ -1,7 +1,7 @@
 const Joi = require('joi')
 const BaseModel = require('../../utils/base-model.js')
 
-module.exports = new BaseModel('Preset', {
+module.exports = new BaseModel('Card', {
   textValue: Joi.string().required(),
   imgValue: Joi.string().required(),
 })
