fa86abf254ca5427772565eba3ea2a9e896db636
CamilleAntonios <camille.antonios@gmail.com>
2024-05-13 09:32:14 +0200

    #147 feat: creation du fichier chargé de la gestion des cartes

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
-
+const cardsRouter = require('./cards')
 const router = new Router()
 router.get('/status', (req, res) => res.status(200).json('ok'))
-
+router.use('/cards', UserRouter)
 module.exports = router
