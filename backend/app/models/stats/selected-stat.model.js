a5e3911a11f98152be14b8bb3f69deeac77a62bc
Gauthier MARTIN <45541503+martgauthier@users.noreply.github.com>
2024-05-20 14:23:10 +0200

    feat: added simple version of stats manager #142 #143

--- backend/app/models/stats/selected-stat.model.js
+++
@@ -1,8 +0,0 @@
-const BaseModel = require("../../utils/base-model")
-const Joi = require("joi");
-
-module.exports = new BaseModel("SelectedStat", {
-    userId: Joi.number().required(),
-    cardId: Joi.number().required(),
-    statType: Joi.string().required()
-});
---
+++ backend/database/generate-stats-mocked-databases.js
@@ -0,0 +1,31 @@
+const generateStatsPerCards = require("./generate-stats-per-cards")
+const generateStatsPerGames = require("./generate-stats-per-games")
+
+const FIRST_TIMESTAMP_TO_GENERATE_FROM = 1684234879533 // 16 mai 2023, il y a plus d'un an
+
+const POSSIBLE_DIFFICULTIES = ["simple", "medium", "hard"]
+
+
+const arrayOfTimestamps=[
+    {
+        timestamp: FIRST_TIMESTAMP_TO_GENERATE_FROM,
+        difficulty: "simple"
+    }];
+
+//crééons 400 parties, jouées depuis 1 an
+for(let i = 0; i < 400; i++) {
+    let lastTimestamp=arrayOfTimestamps[arrayOfTimestamps.length-1].timestamp
+
+    let randomTimestampSurplus=(Math.random() > 0.8) ? 64800000 : 86400000 //1/5 chance of adding 18hours, 4/5 chance of adding a full day before next game
+    let newLastTimestamp=Math.min(new Date().getTime(), lastTimestamp + randomTimestampSurplus)
+    arrayOfTimestamps.push({
+        timestamp: newLastTimestamp,
+        difficulty: POSSIBLE_DIFFICULTIES[Math.floor(Math.random() * 3)]
+    })
+}
+
+
+const USER_IDS = [1,2,3,4,5]
+
+generateStatsPerCards(arrayOfTimestamps, USER_IDS)
+generateStatsPerGames(arrayOfTimestamps, USER_IDS)
