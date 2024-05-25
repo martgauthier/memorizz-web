7f1c8e13e4fc5510e5859babc0a568e1d57e5447
AmelieMuller <amelie17.muller@gmail.com>
2024-05-25 13:57:29 +0200

    feat : back models to fit front models

--- backend/app/models/user/identification.model.js
+++ backend/app/models/user/identification.model.js
@@ -2,9 +2,9 @@ const Joi = require('joi')
 const BaseModel = require('../../utils/base-model.js')
 
 const Identification = new BaseModel('Identification', {
-  id: Joi.number().required(),
   nom: Joi.string().required(),
   prenom: Joi.string().required(),
+  id: Joi.number().required(),
   src: Joi.string(),
 })
 
