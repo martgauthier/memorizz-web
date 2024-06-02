ccfe8f096eebed81922cdc6c553e1d1d23c4c120
CamilleAntonios <camille.antonios@gmail.com>
2024-06-02 12:05:31 +0200

    #183 Ajout d'une image par défaut, image pas affichée dans le header si aucun patient sélectionné

--- backend/app/api/images/index.js
+++ backend/app/api/images/index.js
@@ -8,23 +8,35 @@ router.get("/:idUser/:imageName", (req,res)=>{
     // Checking if the path exists
     
     if(!fs.existsSync("./database/Images/"+req.params.idUser+"/"+req.params.imageName)){
-        res.writeHead(404, {
-            "Content-Type": "text/plain"
-        });
-        res.end("404 Not Found");
-        return;
+        if(req.params.imageName === "pfp.png"){
+            res.writeHead(200, {
+                "Content-Type": "image/png"
+            });
+            fs.readFile("./database/Images/defaultPfp.png",
+                function (err, content) {
+                    // Serving the image
+                    res.end(content);
+                }
+            );
+            return;
+        }else{
+            res.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            res.end("404 Not Found");
+            return;
+        }
+        
     }
     res.writeHead(200, {
         "Content-Type": "image/png"
     });
-
-    // const imagePath = path.join('database', 'Images', req.params.idUser, 're', '..');
-
     fs.readFile("./database/Images/"+req.params.idUser+"/"+req.params.imageName,
         function (err, content) {
             // Serving the image
             res.end(content);
-        });
+        }
+    );
 })
 
 
